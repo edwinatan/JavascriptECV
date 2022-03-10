a105ab55dd94473b106db0a5c63032795c0eb762
edwinatan <edwina.tan@mail-ecv.fr>
2022-03-10 14:41:54 +0100

    Fix exo7 getComputedStyle

--- TP1/Exo7/index.js
+++ TP1/Exo7/index.js
@@ -1,5 +1,5 @@
 const body = document.querySelector("body");
-console.log("height", body.getComputedStyle());
+console.log("height", window.getComputedStyle(body, null).height);
 
 const background = body.style.background;
-console.log("background", background);
+console.log("background", window.getComputedStyle(body, null).background);
